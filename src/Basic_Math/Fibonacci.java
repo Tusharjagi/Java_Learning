de5a6ad636414c6d82be96c9095ba8024a85feea
Tusharjagi <tusharjagi@gmail.com>
2022-03-18 23:03:58 +0530

    First Code In java

--- src/Basic_Math/Fibonacci.java
+++ src/Basic_Math/Fibonacci.java
@@ -4,13 +4,11 @@ import java.util.Scanner;
 
 public class Fibonacci {
 
-    public static void main(String[] args)
-    {
-       int first=0, second = 1 , next, i ,count = 10;
+    public static void main(String[] args) {
+        int first = 0, second = 1, next, i, count = 10;
         System.out.print(first + " " + second);
 
-        for (i = 2; i < count; ++i)
-        {
+        for (i = 2; i < count; ++i) {
             next = first + second;
             System.out.print(" " + next);
             first = second;
@@ -18,8 +16,6 @@ public class Fibonacci {
 
 
         }
-
-
 
 
     }
