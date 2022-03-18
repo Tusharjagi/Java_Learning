70a8b5316adf9388af73a6c87bdce7645b3b6aa3
Tusharjagi <tusharjagi@gmail.com>
2022-03-18 22:51:11 +0530

    Intial commit

---
+++ src/Basic_Math/Fibonacci.java
@@ -0,0 +1,27 @@
+package Basic_Math;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void main(String[] args)
+    {
+       int first=0, second = 1 , next, i ,count = 10;
+        System.out.print(first + " " + second);
+
+        for (i = 2; i < count; ++i)
+        {
+            next = first + second;
+            System.out.print(" " + next);
+            first = second;
+            second = next;
+
+
+        }
+
+
+
+
+    }
+
+}
